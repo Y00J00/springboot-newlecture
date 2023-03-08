d7430b1d1878c262500a86df5dac9c4b89c1c164
Y00J00 <syeunnn@gmail.com>
2023-03-08 09:53:58 +0900

    2023-03-08 1st

--- src/main/java/com/example/demo/dao/NoticeDao.java
+++ src/main/java/com/example/demo/dao/NoticeDao.java
@@ -2,13 +2,16 @@ package com.example.demo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.entity.Notice;
 
+@Mapper
 public interface NoticeDao {
-    @Select("select * from notice")
+    @Select("select * from noticeview")
     List<Notice> getList();
+
     Notice get();
 
 }
